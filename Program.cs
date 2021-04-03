e69ab880ad901cc2ca991b8d01340313ccf2a536
Jorge <jepers2021@gmail.com>
2021-04-03 11:12:37 -0300

    Corrigiendo. Hice el ejercicio 42 en el 41.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace A133590.Ejercicio41
 {
     class Program
     {
+        static DateTime validarIngreso(string ingreso)
+        {
+            DateTime fecha;
+            while (!DateTime.TryParse(ingreso, out fecha))
+            {
+                Console.Write("Ingreso inválido, intente nuevamente: ");
+                ingreso = Console.ReadLine();
+            }
+
+            return fecha;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Ejercicio 41");
-            Console.WriteLine("Programa que recibe una palabra y la presenta al revés.");
-            Console.Write("Por favor, ingrese una palabra: ");
-            char[] palabra = Console.ReadLine().ToCharArray();
-            Array.Reverse(palabra);
-            Console.WriteLine($"Palabra invertida: {new string(palabra)}");
+            Console.WriteLine("Ejercicio 39");
+            Console.WriteLine("Programa que recibe una serie de fechas y luego las presenta en orden ascendente.");
+            List<DateTime> lista = new List<DateTime>();
+            string seguir = "";
+            do
+            {
+                Console.Write("Por favor, ingrese una fecha: ");
+                lista.Add(validarIngreso(Console.ReadLine()));
+                Console.Write("¿Desea ingresar otra fecha? (y/n): ");
+                seguir = Console.ReadLine().Trim();
+            } while (seguir.Equals("y"));
+
+            Console.WriteLine("Lista invertida:");
+            lista.Sort();
+            foreach (DateTime elemento in lista)
+            {
+                Console.WriteLine(elemento);
+            }
+            Console.WriteLine();
             Console.WriteLine("Presione cualquier tecla para continuar..");
             Console.ReadKey();
         }
